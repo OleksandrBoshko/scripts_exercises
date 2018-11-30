67fd1a4da781160fb7a948a8d916de91da138b3d
OleksandrBoshko <oleksandr.boshko@gmail.com>
2018-11-30 21:26:22 +0200

    added exercise1

---
+++ script1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Write a script that upon invocation shows the time and date, lists all logged-in users, and gives the system uptime. The script then saves this information to a logfile.
+
+
+date > cat >> /home/boshko/Devops/scripts_exercises/log.txt
+users > cat >> /home/boshko/Devops/scripts_exercises/log.txt
+uptime > cat >> /home/boshko/Devops/scripts_exercises/log.txt
+
