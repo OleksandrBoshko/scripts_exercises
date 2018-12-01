fed3a1ae9ba09f57841b560551399faffe4d901a
OleksandrBoshko <oleksandr.boshko@gmail.com>
2018-12-01 12:39:42 +0200

    monitors disk space usage, email to admin

--- script4.sh
+++ script4.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-#Write a script that monitors disk space usage, if it's used by 90% send email to admin.
+# 4. Write a script that monitors disk space usage, if it's used by 90% send email to admin.
 
+USED=`df . | awk '{print $5}' | sed -ne 2p | cut -d"%" -f1`
 
+if [ $USED -gt 5 ] then
+echo "E-mail"
+fi
+echo "ok"
