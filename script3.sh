c41a4c10b08589554b14a8f07560b49ec1021375
OleksandrBoshko <oleksandr.boshko@gmail.com>
2018-12-01 12:38:08 +0200

    counts lines of text file, param

--- script3.sh
+++ script3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#Write a script that counts lines of text file  (name of file defines as param of script).
+# 3. Write a script that counts lines of text file  (name of file defines as param of script).
 
 wc -l < $1
