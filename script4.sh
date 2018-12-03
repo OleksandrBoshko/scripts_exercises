3928cdc997b18a2287fc7971ebafba764d4409a4
OleksandrBoshko <oleksandr.boshko@gmail.com>
2018-12-03 17:38:31 +0200

    monitors disk space usage, email to admin. added e-mail

--- script4.sh
+++ script4.sh
@@ -4,7 +4,8 @@
 
 USED=`df . | awk '{print $5}' | sed -ne 2p | cut -d"%" -f1`
 
-if [ $USED -gt 5 ] then
-echo "E-mail"
+if [ $USED -gt 0 ]; then
+ echo "Using space is critical now!" | mail -s subject user@gmail.com
+ exit 1
 fi
-echo "ok"
+
