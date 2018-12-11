0c335011c555b9ef6ab5fd068c2c1d0f63902b7d
Oleksandr <oleksandr.boshko@gmail.com>
2018-12-11 13:13:39 +0200

    check format of the archive and extract content

---
+++ script9.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# 9. Write a sctipt which downloads http://distfiles.gentoo.org/releases/amd64/autobuilds/current-install-amd64-minimal/stage3-x32-20181129T214502Z.tar.xz then check format of the archive and extract content to /tmp. 
+
