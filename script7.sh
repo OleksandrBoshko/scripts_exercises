de38d22d738523d67e338b8b7df0d40f1dbc595e
Oleksandr <oleksandr.boshko@gmail.com>
2018-12-11 13:08:11 +0200

    NEW! cron run every 4 hours and tell if file was changed

--- script7.sh
+++ script7.sh
@@ -3,4 +3,16 @@
 # 7. Write a script which by cron run every 4 hours and tells if /var/log/file (was changed).
 
 
-crontab -e
+# crontab -e
+# 0 */4 * * * /home/boshko/Devops/scripts_exercises/script7.sh
+
+sum2="$(md5sum /var/log/file)"
+
+    if [ "$sum1" = "$sum2" ];
+    then
+        echo "Identical" #  >> /home/boshko/Devops/scripts_exercises/input_file.txt
+    else
+        echo "Different" #  >> /home/boshko/Devops/scripts_exercises/input_file.txt
+    fi
+
+sum1="$(md5sum /var/log/file)"
