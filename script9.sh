4110b665d19275e243ee6bf405625f00f99f434f
Oleksandr <oleksandr.boshko@gmail.com>
2018-12-12 14:26:50 +0200

    Update script9.sh

--- script9.sh
+++ script9.sh
@@ -2,3 +2,4 @@
 
 # 9. Write a sctipt which downloads http://distfiles.gentoo.org/releases/amd64/autobuilds/current-install-amd64-minimal/stage3-x32-20181129T214502Z.tar.xz then check format of the archive and extract content to /tmp. 
 
+#https://stackoverflow.com/questions/26997964/bash-script-to-recognise-archive-format-and-extract-files-from-archives-not-work
